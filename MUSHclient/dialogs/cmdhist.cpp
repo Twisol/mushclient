70c98544dbbe5653514c3f40e1d67ab8914a7b6c
Jonathan Castello <solugon@hotmail.com>
2010-06-14 20:54:26 -0700

    Cleaned up the cmdhist dialog.

--- MUSHclient/dialogs/cmdhist.cpp
+++ MUSHclient/dialogs/cmdhist.cpp
@@ -20,70 +20,65 @@ static char BASED_CODE THIS_FILE[] = __FILE__;
 
 
 CCmdHistory::CCmdHistory(CWnd* pParent /*=NULL*/)
-	: CDialog(CCmdHistory::IDD, pParent)
-{
-	//{{AFX_DATA_INIT(CCmdHistory)
-	//}}AFX_DATA_INIT
-}
+  : CDialog(CCmdHistory::IDD, pParent)
+{}
 
 
 void CCmdHistory::DoDataExchange(CDataExchange* pDX)
 {
-	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CCmdHistory)
-	DDX_Control(pDX, IDC_HISTORY_ITEM, m_ctlHistoryItem);
-	//}}AFX_DATA_MAP
+  CDialog::DoDataExchange(pDX);
+  //{{AFX_DATA_MAP(CCmdHistory)
+  DDX_Control(pDX, IDC_HISTORY_ITEM, m_ctlHistoryItem);
+  //}}AFX_DATA_MAP
 
-if(!(pDX->m_bSaveAndValidate))
+  if(!(pDX->m_bSaveAndValidate))
     {
-CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
-ASSERT (pList);
-int count = 0;
+    CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
+    ASSERT (pList);
+    int count = 0;
 
-   pList->SetRedraw (FALSE);
-   pList->ResetContent ();
+    pList->SetRedraw (FALSE);
+    pList->ResetContent ();
 
-  CString str;
-  POSITION pos = m_msgList->GetHeadPosition ();
-
-  while (pos)
-    {
-    int nItem;
-    POSITION itemPos = pos;
-    str = m_msgList->GetNext (pos);
-
-    // truncate long strings or we might get a nasty crash with long strings
-
-    if (str.GetLength () > 500)
+    POSITION pos = m_msgList->GetHeadPosition ();
+    POSITION itemPos;
+    CString str;
+    while (pos)
       {
-      str = str.Left (500);
-      str += " ...";
+      itemPos = pos;
+      str = m_msgList->GetNext (pos);
+
+      // truncate long strings or we might get a nasty crash with long strings
+      if (str.GetLength () > 500)
+        {
+        str = str.Left (500);
+        str += " ...";
+        }
+
+      int nItem = pList->AddString(str);  // add to list (truncated to 504 chars)
+      if (nItem != LB_ERR && nItem != LB_ERRSPACE)
+        pList->SetItemData (nItem, (DWORD) itemPos);   // remember all about this string
+
+      ++count;
       }
 
-    nItem = pList->AddString(str);  // add to list (truncate to 500 chars)
-    if (nItem != LB_ERR  && nItem != LB_ERRSPACE)
-      pList->SetItemData (nItem, (DWORD) itemPos);   // remember all about this string
-    count++;
-    }
-
-   pList->SetCurSel(count - 1);
-   pList->SetRedraw (TRUE);
-   OnSelchangeCommands ();    // make sure item is displayed in history box
+    pList->SetCurSel(count - 1);
+    pList->SetRedraw (TRUE);
+    OnSelchangeCommands ();    // make sure item is displayed in history box
     }   // end of loading the dialog
-
 }
 
 
 BEGIN_MESSAGE_MAP(CCmdHistory, CDialog)
-	//{{AFX_MSG_MAP(CCmdHistory)
-	ON_LBN_DBLCLK(IDC_COMMANDS, OnDblclkCommands)
-	ON_BN_CLICKED(IDC_HELPBUTTON, OnHelpbutton)
-	ON_BN_CLICKED(IDC_FIND, OnFind)
-	ON_BN_CLICKED(IDC_FIND_NEXT, OnFindNext)
-	ON_LBN_SELCHANGE(IDC_COMMANDS, OnSelchangeCommands)
-	ON_BN_CLICKED(IDC_DO, OnDo)
-	ON_BN_CLICKED(IDC_NOTEPAD, OnNotepad)
-	//}}AFX_MSG_MAP
+  //{{AFX_MSG_MAP(CCmdHistory)
+  ON_LBN_DBLCLK(IDC_COMMANDS, OnDblclkCommands)
+  ON_BN_CLICKED(IDC_HELPBUTTON, OnHelpbutton)
+  ON_BN_CLICKED(IDC_FIND, OnFind)
+  ON_BN_CLICKED(IDC_FIND_NEXT, OnFindNext)
+  ON_LBN_SELCHANGE(IDC_COMMANDS, OnSelchangeCommands)
+  ON_BN_CLICKED(IDC_DO, OnDo)
+  ON_BN_CLICKED(IDC_NOTEPAD, OnNotepad)
+  //}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 
@@ -92,37 +87,34 @@ END_MESSAGE_MAP()
 
 void CCmdHistory::OnOK() 
 {
-CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
-ASSERT (pList);
-int selection = pList->GetCurSel ();
-CString str;
+  CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
+  ASSERT (pList);
 
-if (selection == LB_ERR)
-  return;
+  int selection = pList->GetCurSel ();
+  if (selection == LB_ERR)
+    return;
 
-POSITION pos = (POSITION) pList->GetItemData (selection);
-str = m_msgList->GetAt (pos);
+  POSITION pos = (POSITION) pList->GetItemData (selection);
+  CString str = m_msgList->GetAt (pos);
 
-// check they want to wipe out their typing
+  // check they want to wipe out their typing
+  if (m_sendview->CheckTyping (m_pDoc, str))
+    return;
 
-if (m_sendview->CheckTyping (m_pDoc, str))
-  return;
-
-m_sendview->SetCommand (str);
-	
-	CDialog::OnOK();
+  m_sendview->SetCommand (str);
+  CDialog::OnOK();
 }
 
 void CCmdHistory::OnDblclkCommands() 
 {
-  OnOK ();    // same as clicking on OK button
+  OnOK (); // same as clicking on OK button
 }
 
 
 void CCmdHistory::OnHelpbutton() 
 {
-//	  OnCmdMsg(ID_HELP, CN_COMMAND, NULL, NULL);	
-	App.WinHelp(m_nIDHelp + HID_BASE_RESOURCE);
+//  OnCmdMsg(ID_HELP, CN_COMMAND, NULL, NULL);	
+  App.WinHelp(m_nIDHelp + HID_BASE_RESOURCE);
 }
 
 void CCmdHistory::OnFind() 
@@ -137,49 +129,40 @@ void CCmdHistory::OnFindNext()
 
 
 void CCmdHistory::DoFind (bool bAgain)
-  {
-CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
-ASSERT (pList);
+{
+  CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
+  ASSERT (pList);
 
-m_pHistoryFindInfo->m_bAgain = bAgain;
-m_pHistoryFindInfo->m_nTotalLines = m_msgList->GetCount ();
+  m_pHistoryFindInfo->m_bAgain = bAgain;
+  m_pHistoryFindInfo->m_nTotalLines = m_msgList->GetCount ();
 
-bool found = FindRoutine (m_msgList,             // passed back to callback routines
-                          *m_pHistoryFindInfo,   // finding structure
-                          InitiateSearch,        // how to re-initiate a find
-                          GetNextLine);          // get the next line
-	
-  if (found)
-    pList->SetCurSel (m_pHistoryFindInfo->m_nCurrentLine);
-  else
-    pList->SetCurSel (-1);
+  bool found = FindRoutine (
+      m_msgList,           // passed back to callback routines
+      *m_pHistoryFindInfo, // finding structure
+      InitiateSearch,      // how to re-initiate a find
+      GetNextLine          // get the next line
+      );
 
+  pList->SetCurSel((found) ? m_pHistoryFindInfo->m_nCurrentLine : -1);
   OnSelchangeCommands ();
-
-  } // end of CCmdHistory::DoFind
-
+} // end of CCmdHistory::DoFind
 
 
-void CCmdHistory::InitiateSearch (const CObject * pObject,
-                                  CFindInfo & FindInfo)
-  {
-CStringList* pMsgList = (CStringList*) pObject;
+void CCmdHistory::InitiateSearch (const CObject * pObject, CFindInfo & FindInfo)
+{
+  CStringList* pMsgList = (CStringList*) pObject;
 
   if (FindInfo.m_bAgain)
     FindInfo.m_pFindPosition = pMsgList->FindIndex (FindInfo.m_nCurrentLine);
+  else if (FindInfo.m_bForwards)
+    FindInfo.m_pFindPosition = pMsgList->GetHeadPosition ();
   else
-    if (FindInfo.m_bForwards)
-      FindInfo.m_pFindPosition = pMsgList->GetHeadPosition ();
-    else
-      FindInfo.m_pFindPosition = pMsgList->GetTailPosition ();
+    FindInfo.m_pFindPosition = pMsgList->GetTailPosition ();
+} // end of CSendView::InitiateSearch
 
-  } // end of CSendView::InitiateSearch
-
-bool CCmdHistory::GetNextLine (const CObject * pObject,
-                             CFindInfo & FindInfo, 
-                             CString & strLine)
-  {
-CStringList* pMsgList = (CStringList*) pObject;
+bool CCmdHistory::GetNextLine (const CObject * pObject, CFindInfo & FindInfo, CString & strLine)
+{
+  CStringList* pMsgList = (CStringList*) pObject;
 
   if (FindInfo.m_pFindPosition == NULL)
     return true;
@@ -190,82 +173,68 @@ CStringList* pMsgList = (CStringList*) pObject;
     strLine = pMsgList->GetPrev (FindInfo.m_pFindPosition);
 
   return false;
-  } // end of CSendView::GetNextLine
+} // end of CSendView::GetNextLine
 
 void CCmdHistory::OnSelchangeCommands() 
 {
-CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
-ASSERT (pList);
-int selection = pList->GetCurSel ();
-CString str;
+  CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
+  ASSERT (pList);
 
-if (selection == LB_ERR)
-  {
-  m_ctlHistoryItem.SetWindowText ("");
-  return;
-  }
+  int selection = pList->GetCurSel ();
+  if (selection == LB_ERR)
+    {
+    m_ctlHistoryItem.SetWindowText ("");
+    return;
+    }
 
-pList->GetText (selection, str);
-
-m_ctlHistoryItem.SetWindowText (str);
-	
+  CString str;
+  pList->GetText (selection, str);
+  m_ctlHistoryItem.SetWindowText (str);
 }
 
 
 void CCmdHistory::OnDo() 
 {
+  CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
+  ASSERT (pList);
 
-CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
-ASSERT (pList);
-int selection = pList->GetCurSel ();
-CString str;
-
-if (selection == LB_ERR)
-  return;
-
-POSITION pos = (POSITION) pList->GetItemData (selection);
-str = m_msgList->GetAt (pos);
-
-m_sendview->SendCommand (str, TRUE);
-	
-}
-
-void CCmdHistory::OnNotepad() 
-{
-CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
-ASSERT (pList);
-int selection = pList->GetCurSel ();
-CString str;
-
+  int selection = pList->GetCurSel ();
   if (selection == LB_ERR)
     return;
 
   POSITION pos = (POSITION) pList->GetItemData (selection);
-  str = m_msgList->GetAt (pos);
+  CString str = m_msgList->GetAt (pos);
+  m_sendview->SendCommand (str, TRUE);	
+}
+
+void CCmdHistory::OnNotepad() 
+{
+  CListBox* pList = (CListBox*) GetDlgItem (IDC_COMMANDS);
+  ASSERT (pList);
+
+  int selection = pList->GetCurSel ();
+  if (selection == LB_ERR)
+    return;
+
+  POSITION pos = (POSITION) pList->GetItemData (selection);
+  CString str = m_msgList->GetAt (pos);
 
   // edit current input window
-  CreateTextWindow (str,     // command
-                    TFormat ("Notepad: %s", (LPCTSTR) m_pDoc->m_mush_name),     // title
-                    m_pDoc,   // document
-                    m_pDoc->m_iUniqueDocumentNumber,      // document number
-                    m_pDoc->m_input_font_name,
-                    m_pDoc->m_input_font_height,
-                    m_pDoc->m_input_font_weight,
-                    m_pDoc->m_input_font_charset,
-                    m_pDoc->m_input_text_colour,
-                    m_pDoc->m_input_background_colour,
-                    "",       // search string
-                    "",       // line preamble
-                    false,
-                    false,
-                    false,
-                    false,  
-                    false,
-                    false,
-                    eNotepadNormal
-                    );
+  CreateTextWindow (
+      str,     // command
+      TFormat ("Notepad: %s", (LPCTSTR) m_pDoc->m_mush_name),     // title
+      m_pDoc,   // document
+      m_pDoc->m_iUniqueDocumentNumber,      // document number
+      m_pDoc->m_input_font_name,   m_pDoc->m_input_font_height,
+      m_pDoc->m_input_font_weight, m_pDoc->m_input_font_charset,
+      m_pDoc->m_input_text_colour, m_pDoc->m_input_background_colour,
+      "",       // search string
+      "",       // line preamble
+      false, false, false,
+      false, false, false,
+      eNotepadNormal
+      );
 
   // bye-bye dialog box
-	CDialog::OnOK();
-
+  CDialog::OnOK();
 }

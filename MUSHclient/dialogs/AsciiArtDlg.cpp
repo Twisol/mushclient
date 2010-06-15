44c9535b634937302af89e0d92c977d28323c42b
Jonathan Castello <solugon@hotmail.com>
2010-06-14 20:15:00 -0700

    Cleaned up AsciiArtDlg.

--- MUSHclient/dialogs/AsciiArtDlg.cpp
+++ MUSHclient/dialogs/AsciiArtDlg.cpp
@@ -16,31 +16,25 @@ static char THIS_FILE[] = __FILE__;
 
 
 CAsciiArtDlg::CAsciiArtDlg(CWnd* pParent /*=NULL*/)
-	: CDialog(CAsciiArtDlg::IDD, pParent)
-{
-	//{{AFX_DATA_INIT(CAsciiArtDlg)
-	m_strText = _T("");
-	m_strFont = _T("");
-	m_iLayout = -1;
-	//}}AFX_DATA_INIT
-}
+	: CDialog(CAsciiArtDlg::IDD, pParent),
+      m_strText(_T("")), m_strFont(_T("")), m_iLayout(-1)
+{}
 
 
 void CAsciiArtDlg::DoDataExchange(CDataExchange* pDX)
 {
-	CDialog::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(CAsciiArtDlg)
-	DDX_Control(pDX, IDC_TEXT, m_ctlText);
-	DDX_Control(pDX, IDC_FONT, m_ctlFont);
-	DDX_Text(pDX, IDC_TEXT, m_strText);
-	DDV_MaxChars(pDX, m_strText, 60);
-	DDX_Text(pDX, IDC_FONT, m_strFont);
-	DDX_CBIndex(pDX, IDC_LAYOUT, m_iLayout);
-	//}}AFX_DATA_MAP
+  CDialog::DoDataExchange(pDX);
+  //{{AFX_DATA_MAP(CAsciiArtDlg)
+  DDX_Control(pDX, IDC_TEXT, m_ctlText);
+  DDX_Control(pDX, IDC_FONT, m_ctlFont);
+  DDX_Text(pDX, IDC_TEXT, m_strText);
+  DDV_MaxChars(pDX, m_strText, 60);
+  DDX_Text(pDX, IDC_FONT, m_strFont);
+  DDX_CBIndex(pDX, IDC_LAYOUT, m_iLayout);
+  //}}AFX_DATA_MAP
 
- if(pDX->m_bSaveAndValidate)
-   {
-
+  if (pDX->m_bSaveAndValidate)
+    {
     m_strText.TrimLeft ();
     m_strText.TrimRight ();
 
@@ -60,15 +54,14 @@ void CAsciiArtDlg::DoDataExchange(CDataExchange* pDX)
       DDX_Text(pDX, IDC_FONT, m_strFont);
       pDX->Fail();
       }
-   }
-
+    }
 }
 
 
 BEGIN_MESSAGE_MAP(CAsciiArtDlg, CDialog)
-	//{{AFX_MSG_MAP(CAsciiArtDlg)
-	ON_BN_CLICKED(IDC_BROWSE, OnBrowse)
-	//}}AFX_MSG_MAP
+  //{{AFX_MSG_MAP(CAsciiArtDlg)
+  ON_BN_CLICKED(IDC_BROWSE, OnBrowse)
+  //}}AFX_MSG_MAP
   ON_MESSAGE(WM_KICKIDLE, OnKickIdle)
   ON_UPDATE_COMMAND_UI(IDOK, OnUpdateNeedThings)
 END_MESSAGE_MAP()
@@ -78,32 +71,31 @@ END_MESSAGE_MAP()
 
 void CAsciiArtDlg::OnBrowse() 
 {
-	CFileDialog	dlg(TRUE,						// TRUE for FileOpen
-					"flf",						// default extension
-					GetText (m_ctlFont),						// initial file name
-					OFN_HIDEREADONLY|OFN_FILEMUSTEXIST,
-					"Figlet font files (*.flf)|*.flf|All files (*.*)|*.*||");
-	dlg.m_ofn.lpstrTitle = "Select font file";
+  CFileDialog dlg(
+      TRUE,                // TRUE for FileOpen
+      "flf",               // default extension
+      GetText (m_ctlFont), // initial file name
+      OFN_HIDEREADONLY|OFN_FILEMUSTEXIST,
+      "Figlet font files (*.flf)|*.flf|All files (*.*)|*.*||");
+  dlg.m_ofn.lpstrTitle = "Select font file";
 
   ChangeToFileBrowsingDirectory ();
-	int nResult = dlg.DoModal();
+  int nResult = dlg.DoModal();
   ChangeToStartupDirectory ();
 
-	if (nResult != IDOK)
-		return;
+  if (nResult != IDOK)
+    return;
   m_ctlFont.SetWindowText (dlg.GetPathName());	
 }
 
 
 LRESULT CAsciiArtDlg::OnKickIdle(WPARAM, LPARAM)
-  {
+{
   UpdateDialogControls (AfxGetApp()->m_pMainWnd, false);
   return 0;
-  } // end of CAsciiArtDlg::OnKickIdle
+} // end of CAsciiArtDlg::OnKickIdle
 
 void CAsciiArtDlg::OnUpdateNeedThings(CCmdUI* pCmdUI)
 {
-	pCmdUI->Enable(!GetText (m_ctlText).IsEmpty () &&
-                 !GetText (m_ctlFont).IsEmpty ());
+  pCmdUI->Enable(!GetText (m_ctlText).IsEmpty () && !GetText (m_ctlFont).IsEmpty ());
 }
-
--- MUSHclient/dialogs/AsciiArtDlg.h
+++ MUSHclient/dialogs/AsciiArtDlg.h
@@ -12,38 +12,38 @@
 
 class CAsciiArtDlg : public CDialog
 {
-// Construction
-public:
-	CAsciiArtDlg(CWnd* pParent = NULL);   // standard constructor
+  public:
+    // Construction
+    CAsciiArtDlg(CWnd* pParent = NULL);   // standard constructor
 
-// Dialog Data
-	//{{AFX_DATA(CAsciiArtDlg)
-	enum { IDD = IDD_ASCII_ART };
-	CEdit	m_ctlText;
-	CEdit	m_ctlFont;
-	CString	m_strText;
-	CString	m_strFont;
-	int		m_iLayout;
-	//}}AFX_DATA
+    // Dialog Data
+    //{{AFX_DATA(CAsciiArtDlg)
+    enum { IDD = IDD_ASCII_ART };
+    CEdit   m_ctlText;
+    CEdit   m_ctlFont;
+    CString m_strText;
+    CString m_strFont;
+    int     m_iLayout;
+    //}}AFX_DATA
 
 
-// Overrides
-	// ClassWizard generated virtual function overrides
-	//{{AFX_VIRTUAL(CAsciiArtDlg)
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+    // Overrides
+    // ClassWizard generated virtual function overrides
+    //{{AFX_VIRTUAL(CAsciiArtDlg)
+  protected:
+    virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
-// Implementation
-protected:
+    // Implementation
+  protected:
+    // Generated message map functions
+    //{{AFX_MSG(CAsciiArtDlg)
+    afx_msg void OnBrowse();
+    //}}AFX_MSG
+    afx_msg LRESULT OnKickIdle(WPARAM, LPARAM);
+    afx_msg void OnUpdateNeedThings(CCmdUI* pCmdUI);
 
-	// Generated message map functions
-	//{{AFX_MSG(CAsciiArtDlg)
-	afx_msg void OnBrowse();
-	//}}AFX_MSG
-  afx_msg LRESULT OnKickIdle(WPARAM, LPARAM);
-  afx_msg void OnUpdateNeedThings(CCmdUI* pCmdUI);
-	DECLARE_MESSAGE_MAP()
+    DECLARE_MESSAGE_MAP()
 };
 
 //{{AFX_INSERT_LOCATION}}

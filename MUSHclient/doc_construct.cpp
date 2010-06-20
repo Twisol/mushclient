Twisol/mushclient

commit 5286004e8fe86b19bb31495e829ddded9fe7490b

Changed CActiveTagList to be a vector instead of a CTypedPtrList.

MUSHclient/doc_construct.cpp
@@ -540,7 +540,9 @@ int i;
 
 // delete active tags list
 
-  DELETE_LIST (m_ActiveTagList);
+  for (ActiveTagsIterator itr = m_ActiveTagList.begin(); itr != m_ActiveTagList.end(); ++itr)
+    delete *itr;
+  m_PluginList.clear();
 
 // delete actions list
 

MUSHclient/mxp/mxpOnOff.cpp
@@ -166,7 +166,9 @@ void CMUSHclientDoc::MXP_On (const bool bPueblo, const bool bManual)
 
   // delete active tags list
 
-    DELETE_LIST (m_ActiveTagList);
+    for (ActiveTagsIterator itr = m_ActiveTagList.begin(); itr != m_ActiveTagList.end(); ++itr)
+      delete *itr;
+    m_PluginList.clear();
 
   // delete custom entities list
 

MUSHclient/OtherTypes.h
@@ -726,7 +726,7 @@ typedef CTypedPtrMap <CMapStringToPtr, CString, CColours*> CColoursMap;
 /////////////////////////////////////////////////////////////////////////////
 //  CActiveTag - these are outstanding (unclosed) tags
 
-class CActiveTag :public CObject
+class CActiveTag : public CObject
 {
   public:
     CString strName; // name of tag we opened
@@ -738,7 +738,8 @@ class CActiveTag :public CObject
     {}
 };
 
-typedef CTypedPtrList <CPtrList, CActiveTag*> CActiveTagList;
+typedef vector<CActiveTag*> CActiveTagList;
+typedef vector<CActiveTag*>::iterator ActiveTagsIterator;
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -746,7 +747,7 @@ typedef CTypedPtrList <CPtrList, CActiveTag*> CActiveTagList;
 
 class CScriptEngine;
 
-class CPlugin :public CObject
+class CPlugin : public CObject
 {
   public:
     // methods

MUSHclient/dialogs/world_prefs/configuration.cpp
@@ -555,7 +555,7 @@ void CMUSHclientDoc:: LoadPrefsP15 (CPrefsP15 &page15)
   page15.m_strMXPinbuiltEntities  = CFormat ("%i", App.m_EntityMap.GetCount ());     
   page15.m_strMXPmudElements      = CFormat ("%i", m_CustomElementMap.GetCount ());         
   page15.m_strMXPmudEntities      = CFormat ("%i", m_CustomEntityMap.GetCount ());         
-  page15.m_strMXPunclosedTags     = CFormat ("%i", m_ActiveTagList.GetCount ());        
+  page15.m_strMXPunclosedTags     = CFormat ("%i", m_ActiveTagList.size ());        
   page15.m_strMXPactionsCached    = CFormat ("%i", m_ActionList.GetCount ());        
   page15.m_strMXPreferenceCount   = CFormat ("%i", iRefCount);        
 

MUSHclient/mxp/mxpClose.cpp
@@ -400,9 +400,9 @@ void CMUSHclientDoc::MXP_CloseOpenTags (void)
 
 // see if we know of this element
 
-  while (!m_ActiveTagList.IsEmpty ())
+  while (!m_ActiveTagList.empty ())
     {
-    CActiveTag * pTag = m_ActiveTagList.GetTail ();
+    CActiveTag * pTag = m_ActiveTagList.back ();
 
     // don't close securely-opened tags here
     if (pTag->bSecure)
@@ -415,7 +415,7 @@ void CMUSHclientDoc::MXP_CloseOpenTags (void)
               (LPCTSTR) strTag)); 
     MXP_CloseTag (strTag);
 
-    m_ActiveTagList.RemoveTail ();  // get rid of it
+    m_ActiveTagList.pop_back ();  // get rid of it
     delete pTag;
     }
 
@@ -423,9 +423,9 @@ void CMUSHclientDoc::MXP_CloseOpenTags (void)
 
 void CMUSHclientDoc::MXP_CloseAllTags (void)
   {
-  while (!m_ActiveTagList.IsEmpty ())
+  while (!m_ActiveTagList.empty ())
     {
-    CActiveTag * pTag = m_ActiveTagList.GetTail ();
+    CActiveTag * pTag = m_ActiveTagList.back ();
 
     // if protected from reset, stop closing
     if (pTag->bNoReset)
@@ -435,7 +435,7 @@ void CMUSHclientDoc::MXP_CloseAllTags (void)
               TFormat ("<reset> closure of MXP tag: <%s>", 
               (LPCTSTR) pTag->strName)); 
     MXP_CloseTag (pTag->strName);
-    m_ActiveTagList.RemoveTail ();
+    m_ActiveTagList.pop_back ();
     delete pTag;
     }
   } // end of CMUSHclientDoc::MXP_CloseAllTags

MUSHclient/mxp/mxpEnd.cpp
@@ -53,9 +53,11 @@ CString strName;
 
   CActiveTag * pTag = NULL;
 
-  for (POSITION pos = m_ActiveTagList.GetTailPosition (); pos; )
+  ActiveTagsIterator itr = m_ActiveTagList.end();
+  while (itr != m_ActiveTagList.begin()) // reverse iteration
     {
-    pTag = m_ActiveTagList.GetPrev (pos);
+    --itr;
+    pTag = *itr;
 
     if (pTag->strName == strName)
       break;
@@ -73,7 +75,6 @@ CString strName;
         }
       pTag = NULL;
       }
-
     } // end of doing each one
 
   if (!pTag)
@@ -99,8 +100,8 @@ CString strName;
 
   while (true)
     {
-
-    pTag = m_ActiveTagList.RemoveTail ();
+    pTag = m_ActiveTagList.back ();
+    m_ActiveTagList.pop_back ();
     CString strTag = pTag->strName;
     delete pTag;
     

MUSHclient/mxp/mxpStart.cpp
@@ -206,17 +206,18 @@ CString strVariable;
     pTag->strName = strName;
     pTag->bSecure = bSecure;
     pTag->bNoReset = bNoReset;
-    m_ActiveTagList.AddTail (pTag);  // add to outstanding tag list
+    m_ActiveTagList.push_back (pTag);  // add to outstanding tag list
 
     // warn if they are overdoing the outstanding tags
-    if (m_ActiveTagList.GetCount () % OUTSTANDING_TAG_WARNING == 0 &&
-        m_ActiveTagList.GetCount () != m_iLastOutstandingTagCount)
+    size_t size = m_ActiveTagList.size ();
+    if ((size % OUTSTANDING_TAG_WARNING) == 0 &&
+        size != m_iLastOutstandingTagCount)
       {
       MXP_error (DBG_WARNING, wrnMXP_ManyOutstandingTags,
                   TFormat (
                           "Now have %i outstanding MXP tags" ,
-                          m_ActiveTagList.GetCount ()));
-      m_iLastOutstandingTagCount = m_ActiveTagList.GetCount ();
+                          size));
+      m_iLastOutstandingTagCount = size;
       }
 
 

commit 371500437aafbd0a2ce72202c8b3391082df17c6

Fixed a bug that caused the list of plugins to be wiped after disconnecting and reconnecting to a world.
Broken in 5286004e8fe86b19bb31495e829ddded9fe7490b.

MUSHclient/doc_construct.cpp
@@ -544,7 +544,7 @@ int i;
 
   for (ActiveTagsIterator itr = m_ActiveTagList.begin(); itr != m_ActiveTagList.end(); ++itr)
     delete *itr;
-  m_PluginList.clear();
+  m_ActiveTagList.clear();
 
 // delete actions list
 

MUSHclient/mxp/mxpOnOff.cpp
@@ -170,7 +170,7 @@ void CMUSHclientDoc::MXP_On (const bool bPueblo, const bool bManual)
 
     for (ActiveTagsIterator itr = m_ActiveTagList.begin(); itr != m_ActiveTagList.end(); ++itr)
       delete *itr;
-    m_PluginList.clear();
+    m_ActiveTagList.clear();
 
   // delete custom entities list
 